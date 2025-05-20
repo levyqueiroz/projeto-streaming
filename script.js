80c2a79fb010e1841403284a47e010bc1940d59c
André Levy <queiroz.csc7@gmail.com>
2025-05-20 20:51:48 -0300

    Att

---
+++ script.js
@@ -0,0 +1,9 @@
+function rolarCarrossel(direcao) {
+  const carrossel = document.getElementById('carrossel');
+  const distanciaRolagem = 300; 
+
+  carrossel.scrollBy({
+    left: direcao * distanciaRolagem,
+    behavior: 'smooth'
+  });
+}
