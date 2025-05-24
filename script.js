fb5642616bbcc9be5c49f4583e8dcaf4bc2ada96
André Levy <queiroz.csc7@gmail.com>
2025-05-24 20:19:28 -0300

    Att

--- script.js
+++ script.js
@@ -16,6 +16,16 @@ function rolarCarrossel2(direcao) {
     left: direcao * distanciaRolagem,
     behavior: 'smooth'
   });
+
 }
 
+const menuDiv = document.getElementById('menu-mobile')
+const btnAnimar = document.getElementById('btn-menu')
+
+menuDiv.addEventListener('click', animarMenu)
+
+function animarMenu() {
+  menuDiv.classList.toggle('abrir')
+  btnAnimar.classList.toggle('ativo')
+}
 
